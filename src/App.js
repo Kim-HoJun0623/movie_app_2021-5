29fa9b18f6ada31515bd9220f4dab0c36af94e33
김호준 <rlaghwns126@naver.com>
2021-11-06 18:04:47 +0900

    오늘의 복습

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import "./App.css"
 import {HashRouter, Route} from 'react-router-dom'
 import About from './routes/About'
 import Home from './routes/Home'
+import Navigation from "./components/Navigation"
+import Detail from "./routes/Detail"
 
 function App(){
   return(
    <HashRouter> 
-      <Route path='/home' component={Home}/>
+      <Navigation />
+      <Route path='/'exact={true}  component={Home}/>
       <Route path='/about' component={About}/>
+      <Route path='/movie-detail' component={Detail} />
    </HashRouter>
   )
 }
 
 export default App;
-
- {/* <Route path='/' exact='true'  component={Home} >
-        <h1>Home</h1>
-        </Route>
-      <Route path='/about'  component={About} /> */}
--- src/routes/About.js
+++ src/routes/About.js
@@ -1,6 +1,6 @@
 import './About.css'
 console.log("about");
-function About(){
+function About(props) {
     return(
         <div className='about-container'>
            <span> <h1>Hello About</h1></span>
